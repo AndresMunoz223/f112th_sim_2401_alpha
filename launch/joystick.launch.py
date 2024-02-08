32599a8640f99c165552d9d867b016fc3470886b
Sergio <sergio.gomez88@hotmail.com>
2024-02-08 15:46:37 -0500

    Mux config added, joystick launch teleop node modified

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -26,13 +26,18 @@ def generate_launch_description():
     #                name="teleop_node",
     #                parameters=[joy_params],
     #)
+    #teleop_node = Node(package='teleop_twist_joy', 
+    #                executable='teleop_node',
+    #                name="teleop_node",
+    #                parameters=[joy_params],
+    #                remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+    #)
     teleop_node = Node(package='teleop_twist_joy', 
                     executable='teleop_node',
                     name="teleop_node",
                     parameters=[joy_params],
-                    remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+                    remappings=[('/cmd_vel','/cmd_vel_joy')]
     )
-
 
     # Launch them all!
     return LaunchDescription([
